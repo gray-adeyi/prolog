28cae08a695000e2f75cb386b54c72df56292bef
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-28 16:19:33 +0100

    Update test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -36,6 +36,8 @@ func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Confi
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile: config.ClientCertFile,
+		KeyFile: config.ClientKeyFile,
 		CAFile: config.CAFile,
 	})
 	require.NoError(t, err)
@@ -50,6 +52,7 @@ func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Confi
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server: true,
 	})
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
