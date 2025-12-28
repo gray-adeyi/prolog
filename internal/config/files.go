9bce6f05bf2eab2bbbf1270ed5051ca8943f918f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-28 14:00:24 +0100

    Implement tls config

---
+++ internal/config/files.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	CAFile         = configFile("ca.pem")
+	ServerCertFile = configFile("server.pem")
+	ServerKeyFile  = configFile("server-key.pem")
+)
+
+func configFile(filename string) string {
+	return filepath.Join(os.Getenv("CONFIG_DIR"), filename)
+}
